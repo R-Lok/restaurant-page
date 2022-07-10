2c6a2a4600e6a3b21ffa2a7026ccb7f04af98b08
Ryan Lok <rlok.pc@gmail.com>
2022-07-10 22:03:07 +0800

    Add functions to aid development process

--- src/menu.js
+++ src/menu.js
@@ -29,13 +29,51 @@ function loadMenuPage() {
 
     contentContainer.appendChild(header)
 
-    let menuContainer = document.createElement('div')
+    let menuContainer = newDiv()
     menuContainer.id = 'menu-container'
 
+    let menu = newDiv()
+    menu.id = 'menu'
+
+    let menuTitle = newDiv()
+    menuTitle.classList.add('title')
+    menuTitle.innerText = "Menu"
+
+    let mainDishContainer = newDiv()
+    let mainDishSectionTitle = newDiv()
+    mainDishContainer.classList.add('menu-section-title')
+    mainDishContainer.innerText = 'Main Dishes'
+    mainDishContainer.append(mainDishSectionTitle)
+
+    menu.append(menuTitle, mainDishContainer)
+    mainDishContainer.appendChild(Dish('Beef Roast', 'Delicious beef roast with our signature berry sauce', 20))
+    menuContainer.appendChild(menu)
     contentContainer.appendChild(menuContainer)
 
-    //finish menu html contents
-    
+    function newDiv() {
+        return document.createElement('div')
+    }
+
+    function Dish(name, description, price){
+        let dishContainer = newDiv()
+        dishContainer.classList.add('dish-container')
+        
+        let dishName = newDiv()
+        dishName.classList.add('dish-name')
+        dishName.innerText = name
+
+        let dishDescription = newDiv()
+        dishDescription.classList.add('dish-description')
+        dishDescription.innerText = description
+
+        let dishPrice = newDiv()
+        dishPrice.classList.add('dish-price')
+        dishPrice.innerText = `$${price}`
+
+        dishContainer.append(dishName, dishDescription, dishPrice)
+
+        return dishContainer
+    }
 }
 
 export {loadMenuPage}
